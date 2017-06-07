b95dc6bef3a1e4d33779d85e5095afe57288881b
張子晏 <try_love_tom@icloud.com>
2017-06-07 11:51:49 +0800

    delete end point

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test' });
+  res.render('index', { title: 'The Art Gallery' });
 });
 
 module.exports = router;
